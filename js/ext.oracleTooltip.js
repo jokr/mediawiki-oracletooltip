f9fd920217af222f85742f83e5252830d49c669d
Joel Micha Krebs <joel.krebs@students.unibe.ch>
2013-05-11 15:59:44 +0200

    first implementation working for wiki families, no caching yet

---
+++ js/ext.oracleTooltip.js
@@ -0,0 +1,36 @@
+var cards = jQuery('.mtg-card');
+cards.click(function () {
+    agent = navigator.userAgent;
+    windowName = "Sitelet";
+    params  = "";
+    params += "toolbar=1,";
+    params += "location=1,";
+    params += "directories=0,";
+    params += "status=0,";
+    params += "menubar=0,";
+    params += "scrollbars=1,";
+    params += "resizable=1,";
+    params += "width=800,";
+    params += "height=670";
+
+    var cardName = jQuery(this).html();
+
+    window.open("http://gatherer.wizards.com/Pages/Card/Details.aspx?name="+cardName, windowName, params);
+});
+
+cards.tooltip(
+    {
+        items:'.mtg-card',
+        tooltipClass:'oracle-tooltip',
+        position: {my: "left top+5", at: "left bottom", collision: "flipfit"},
+        content: function(callback) {
+            var cardname = jQuery(this).html();
+            var url = jQuery(this).attr("src");
+            jQuery.get(url, {
+                name: cardname
+            }, function(data) {
+                callback(data);
+            });
+        }
+    }
+);
---
+++ oracleTooltip.php
@@ -0,0 +1,51 @@
+<?php
+if (!defined('MEDIAWIKI')) {
+    echo("This is an extension to the MediaWiki package and cannot be run standalone.\n");
+    die(-1);
+}
+
+$wgExtensionCredits['parser'][] = array(
+    'path' => __FILE__,
+    'name' => 'Oracle Tooltips',
+    'version' => '2.0',
+    'author' => 'Thomas Marchiori, Joel Krebs',
+    'url' => 'http://wiki.magicjudges.org/',
+    'description' => 'This is an extension to link oracle text of Magic: The Gathering cards to the oracle and display a tooltip.'
+);
+
+$wgHooks['ParserFirstCallInit'][] = 'parserInit';
+$wgHooks['BeforePageDisplay'][] = 'loadResources';
+
+$wgResourceModules['jquery.ui.tooltip'] = array(
+    'scripts' => '/jquery.ui.tooltip.js',
+    'dependencies' => array('jquery.ui.core', 'jquery.ui.widget', 'jquery.ui.position'),
+    'localBasePath' => dirname(__FILE__).'/../JavaScript',
+    'remoteExtPath' => 'JavaScript'
+);
+
+$wgResourceModules['ext.oracleTooltip'] = array(
+    'scripts' => 'js/ext.oracleTooltip.js',
+    'styles' => 'css/ext.oracleTooltip.css',
+    'dependencies' => 'jquery.ui.tooltip',
+    'localBasePath' => __DIR__,
+    'remoteExtPath' => 'OracleTooltip'
+);
+
+function parserInit(Parser $parser)
+{
+    $parser->setHook('c', 'render');
+    return true;
+}
+
+function loadResources(OutputPage $out)
+{
+    $out->addModules(array('ext.oracleTooltip', 'prototype'));
+    return true;
+}
+
+function render($input, array $parameters, Parser $parser)
+{
+    $server = $parser->getVariableValue('server');
+    $script = $parser->getVariableValue('scriptpath');
+    return '<a class="mtg-card" src="'.$server.$script.'/extensions/OracleTooltip/js/txtoracle.php">' . htmlspecialchars($input) . '</a>';
+}
